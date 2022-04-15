bf8731c15f18dc9d935704eeaf49b1e8ef9e908d
Carolina Nonato <csnonato@gmail.com>
2022-04-15 18:43:25 -0400

    nav style inline

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,19 +1,24 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom'
 const Nav = () => {
     return (
-        <div>
-            <div>
-                <h1>Recipe <span>App</span></h1>
+        <div style={{
+            width: '100%', height: '10vh', background: 'black', color: 'white', display: 'flex', alignItems: 'center', justifyContent: 'space-between',
+        }}>
+            <div >
+                <h1 style={{
+                    marginLeft: '20px', fontSize: '30px'
+                }}>Recipe <span style={{ color: 'peachPuff' }}>App</span></h1>
             </div>
 
-            <div>
-                <ul>
-                    <li>Home</li>
-                    <li>Categories</li>
+            <div >
+                <ul style={{ display: 'flex', textDecoration: 'none' }}>
+                    <Link to="/" style={{ textDecoration: 'none' }}><li style={{ color: 'white', textDecoration: 'none', listStyle: 'none', margin: '0px 10px', fontSize: "22px" }}>Home</li></Link>
+
+                    <Link to="/categories" style={{ textDecoration: 'none' }}><li style={{ color: 'white', textDecoration: 'none', listStyle: 'none', margin: '0px 10px', fontSize: "22px", marginRight: "30px" }}>Categories</li></Link>
                 </ul>
             </div>
-        </div>
+        </div >
     )
 }
 
